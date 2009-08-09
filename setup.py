cd1c2e826f69585de003b2425ee227216118d578
evilbungle <evilbungle@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-08-09 12:07:12 +0000

    Updating setup.py to use src directory
    git-svn-id: http://svn.plone.org/svn/collective/buildout/collective.recipe.funkload/trunk@94185 db7f04ef-aaf3-0310-a811-c281ed44c4ad

--- setup.py
+++ setup.py
@@ -8,31 +8,9 @@ from setuptools import setup, find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0'
+version = '0.1'
 
-long_description = (
-    read('README.txt')
-    + '\n' +
-    'Detailed Documentation\n'
-    '**********************\n'
-    + '\n' +
-    read('collective', 'recipe', 'funkload', 'README.txt')
-    + '\n' +
-    'Contributors\n' 
-    '************\n'
-    + '\n' +
-    read('CONTRIBUTORS.txt')
-    + '\n' +
-    'Change history\n'
-    '**************\n'
-    + '\n' + 
-    read('CHANGES.txt')
-    + '\n' +
-   'Download\n'
-    '********\n'
-    )
-entry_point = 'collective.recipe.funkload:Recipe'
-entry_points = {"zc.buildout": ["default = %s" % entry_point]}
+long_description = ""    
 
 
 
@@ -55,7 +33,8 @@ setup(name='collective.recipe.funkload',
       author_email='',
       url='',
       license='ZPL',
-      packages=find_packages(exclude=['ez_setup']),
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir = {'':'src'},
       namespace_packages=['collective', 'collective.recipe'],
       include_package_data=True,
       zip_safe=False,
